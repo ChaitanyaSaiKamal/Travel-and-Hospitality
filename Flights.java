68928b91fdab020673a61440adf02a12766ee1e2
Kamal <166135775+ChaitanyaSaiKamal@users.noreply.github.com>
2025-04-12 20:55:48 +0530

    Update Flights.java

--- Flights.java
+++ Flights.java
@@ -3,5 +3,7 @@ public class FlightBook
     public static void main (String [] args)
     {
       System.out.println(" Flight is Booked");
+      System.out.println(" Pick a date");
+      System.out.Println(" Enter a seat num");
     }
   }
